25d95aefc3c227f68c29b94f59b064dfb74e9f20
Eng-Prud <Prudence.n826@gmai.com>
2023-11-28 11:36:27 -0800

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for p in range(1, 101):
+        if p % 3 == 0 and p % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif p % 3 == 0:
+            print("Fizz", end=" ")
+        elif p % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(p, end=" ")
+
+if __name__ == "__main__":
+    fizzbuzz()
+    print("")
