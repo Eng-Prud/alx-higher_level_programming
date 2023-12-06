056b7b5c67ea1038671071c193eb8d53c6720c76
Eng-Prud <Prudence.n826@gmai.com>
2023-12-06 15:48:14 -0800

    Change syntax on task 7 and 8

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
