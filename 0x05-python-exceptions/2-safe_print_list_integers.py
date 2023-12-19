bec908a322d5e7160eb4b499c9e47e697147a39f
Eng-Prud <Prudence.n826@gmai.com>
2023-12-19 09:30:12 -0800

    print first x  elements

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    prints the first x elements of a list and only integers
+    Returns the real number of integers printed
+    """
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return printed
