4f4a75f085533c4e60c490de7b4f7ce9c7493a12
Eng-Prud <Prudence.n826@gmai.com>
2023-12-19 09:41:59 -0800

    Divide elemnt by element

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    prints the first x elements of a list and only integers
-    Returns the real number of integers printed
-    """
-    i = 0
-    printed = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            printed += 1
-        except (ValueError, TypeError):
-            continue
-    print()
-    return printed
+    count = 0
+    try:
+        iterator = iter(my_list)
+        for _ in range(x):
+            element = next(iterator)
+            if type(element) == int:
+                print("{:d}".format(element), end="")
+                count += 1
+    except StopIteration:
+        pass
+    finally:
+        print()
+        return count
