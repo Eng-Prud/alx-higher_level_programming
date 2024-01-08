83f9c64d422b881e66ff4795034dda50a4b2fea2
Eng-Prud <Prudence.n826@gmai.com>
2024-01-08 11:12:51 -0800

    Fix pycodestyle error

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,18 +3,25 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix.
 
     Prototype: def matrix_divided(matrix, div):
-    matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception
-    Each row of the matrix must be of the same size, otherwise raise a TypeError exception
-    div must be a number (integer or float), otherwise raise a TypeError exception
-    div can’t be equal to 0, otherwise raise a ZeroDivisionError exception
-    All elements of the matrix should be divided by div, rounded to 2 decimal places
+    matrix must be a list of lists of integers or floats, otherwise
+    raise a TypeError exception
+    Each row of the matrix must be of the same size, otherwise
+    raise a TypeError exception
+    div must be a number (integer or float), otherwise
+    raise a TypeError exception
+    div can’t be equal to 0, otherwise raise a ZeroDivisionError
+    exception
+    All elements of the matrix should be divided by div, rounded
+    to 2 decimal places
     Returns a new matrix
     You are not allowed to import any module
     """
 
     # Validate matrix input
-    if not all(isinstance(row, list) for row in matrix) or not all(all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix) or not all(
+    all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        "of integers/floats")
 
     # Validate matrix size
     if not all(len(row) == len(matrix[0]) for row in matrix):
