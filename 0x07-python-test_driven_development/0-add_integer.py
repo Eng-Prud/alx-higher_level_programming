bbf26d5aed5d6b944a35db180af705fdfc76b931
Eng-Prud <Prudence.n826@gmai.com>
2024-01-08 10:38:20 -0800

    Divide elements of a matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,26 @@
-!/usr/bin/python3
-"""
-This is the "0-add_integer" module.
-The 0-add_integer module supplies one function, add_integer(a, b).
-"""
+def add_integer(a, b=98):
+    """
+    Adds two integers.
 
+    Prototype: def add_integer(a, b=98):
+    a and b must be integers or floats, otherwise raise a TypeError exception with the message
+    a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    You are not allowed to import any module
+    """
 
-def add_integer(a, b):
-    """Return the addition of two numbers."""
-    if type(a) is not int and type(a) is not float:
+    # Check if a is not an int or float
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+
+    # Check if b is not an int or float
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a
+
+    # Cast a and b to integers
+    a = int(a)
+    b = int(b)
+
+    # Return the sum of a and b
+    return a + b
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python3
-"""
-This is the "2-matrix_divided" module.
-The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
-"""
-
-
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
-    if type(matrix) is not list:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-    size = None
-    for lista in matrix:
-        if type(lista) is not list:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
-        if size is None:
-            size = len(lista)
-        elif size != len(lista):
-            raise TypeError("Each row of the matrix must have the same size")
-        for i in lista:
-            if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-    if type(div) is not int and type(div) is not float:
+    """
+    Divides all elements of a matrix.
+
+    Prototype: def matrix_divided(matrix, div):
+    matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception
+    Each row of the matrix must be of the same size, otherwise raise a TypeError exception
+    div must be a number (integer or float), otherwise raise a TypeError exception
+    div can’t be equal to 0, otherwise raise a ZeroDivisionError exception
+    All elements of the matrix should be divided by div, rounded to 2 decimal places
+    Returns a new matrix
+    You are not allowed to import any module
+    """
+
+    # Validate matrix input
+    if not all(isinstance(row, list) for row in matrix) or not all(all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Validate matrix size
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    # Validate div input
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
+    # Validate div is not zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in lista] for lista in matrix]
+
+    # Divide each element of the matrix by div, rounded to 2 decimal places
+    result_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
+
+    return result_matrix
