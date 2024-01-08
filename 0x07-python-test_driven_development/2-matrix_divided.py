2a928223807cdb5909f87e3fe513e1640ff86ef5
Eng-Prud <Prudence.n826@gmai.com>
2024-01-08 09:37:34 -0800

    Rectify mandatory tasks codes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""Defines a matrix division function."""
+"""
+This is the "2-matrix_divided" module.
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
+"""
+
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix.
-    
-    Args:
-        matrix (list): A list of lists of ints or floats.
-        div (int/float): The divisor.
-    
-    Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
-    
-    Returns:
-        A new matrix representing the result of the division.
-    """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float)) for row in matrix for ele in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    
-    if not isinstance(div, (int, float)):
+    """Divides all elements in the matrix by div"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    size = None
+    for lista in matrix:
+        if type(lista) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(lista)
+        elif size != len(lista):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in lista:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
-    return [[round(num / div, 2) for num in row] for row in matrix]
+    return [[round(i / div, 2) for i in lista] for lista in matrix]
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,25 @@
-#!/bin/python3
-"""Defines a text indentation function."""
+#!/usr/bin/python3
+"""
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
+"""
+
 
 def text_indentation(text):
-    """Print text with two new lines after each '7', '.', or ':'.
-    
-    Args:
-        text (string): The text to print.
-        
-    Raises:
-        TypeError: If text is not a string.
-    """
-    if not isinstance(text, str):
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-    
-    c = 0
-    while c < len(text):
-        print(text[c], end="")
-        
-        if text[c] in ".?:":  # Check for sentence-ending characters
-            print("\n\n", end="")
-            c += 1
-            continue
-        
-        if text[c] == '7':  # Check for '7'
-            print("\n\n", end="")
-        
-        c += 1
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
