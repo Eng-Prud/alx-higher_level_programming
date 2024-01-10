436e17912ff1437eb689ef44b2cdf7aaf53728b2
Eng-Prud <Prudence.n826@gmai.com>
2024-01-10 12:41:22 -0800

    Add file with function that reads file and prints to stdoutput

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for reading a text file(UTP8)and printing it to standard output
+"""
+
+def read_file(filename=""):
+    """
+    Reads file(UTF8)and prints it
+
+    Args:
+        filename(str): name of file being read
+
+        Returns:
+            None
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
+
+if __name__ == "__main__":
+    read_file()
